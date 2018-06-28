7b96de3c17de4817961ad3d203a271bad97222eb
Jason <jdudash@redhat.com>
2018-06-28 15:28:11 -0400

    working on some mobile screen sizes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import logo from './logo.svg';
 import openshiftlogo from './openshift.png';
 import devlogo from './rhdevlogo.svg';
 import octocat from './octocat.png';
-import rhoar from './RHOAR-Logo.png';
 import './App.css';
 import {Netmask} from 'netmask'
 //import ip from 'ip'
@@ -29,7 +28,7 @@ class App extends Component {
 
   handleWindowSizeChange = () => {
     this.setState({ width: window.innerWidth });
-    const isMobile = this.state.width <= 500;
+    const isMobile = this.state.width <= 665;
     this.setState({ isMobile: isMobile });
   };
 
@@ -59,8 +58,44 @@ class App extends Component {
             <img src={openshiftlogo} className="App-logo" alt="openshift" />
             <h1 className="App-title">Let's find that CIDR</h1>
           </header>
-          <p className="App-intro">Please resize the window or rotate your phone.</p>
-          <p className="App-intro">The smaller screen formatting for this webapp is not yet completed.</p>
+          <p className="App-intro">Please resize the window or rotate your phone for full screen.</p>
+          <p className="App-intro">(reformatting for smaller screen size)</p>
+
+          <div className="IPAddressSmall">
+            <span className="quad">
+              <input className="quad" type="number" pattern="[0-9]*" value={this.state.inputcidr[0]} quad-index={0} onChange={this.handleIPChange}/>
+              <span className="dot">.</span>
+            </span>
+            <span className="quad">
+              <input className="quad" type="number" pattern="[0-9]*" value={this.state.inputcidr[1]} quad-index={1} onChange={this.handleIPChange}/>
+              <span className="dot">.</span>
+            </span>
+            <span className="quad">
+              <input className="quad" type="number" pattern="[0-9]*" value={this.state.inputcidr[2]} quad-index={2} onChange={this.handleIPChange}/>
+              <span className="dot">.</span>
+            </span>
+            <span className="quad">
+              <input className="quad" type="number" pattern="[0-9]*" value={this.state.inputcidr[3]} quad-index={3} onChange={this.handleIPChange}/>
+              <span className="dot">/</span>
+            </span>
+            <input className="mask" type="number" pattern="[0-9]*" value={this.state.inputmask} quad-index="mask" onChange={this.handleIPChange}/>
+          </div>
+
+          <NetmaskDetails details={netmaskDetails} issmall={"Small"}></NetmaskDetails>
+
+          <section>
+            <div class="developer-links">
+              <a href="https://github.com/dudash/cidr-finder"><img src={octocat} height="50" className="octocat" alt="github" /></a>
+              <a href="https://developers.redhat.com"><img src={devlogo} height="50" className="devslogo" alt="devslink" /></a>
+            </div>
+          </section>
+
+          <section>
+            <div class="wiki-link">
+            Confused? Maybe you need <a href="https://en.wikipedia.org/wiki/Classless_Inter-Domain_Routing#CIDR_notation">a refresher on what is CIDR notation?</a>
+            </div>
+          </section>
+
         </div>
       );
     } else {
@@ -103,13 +138,12 @@ class App extends Component {
             </ol>
           </div>
 
-          <NetmaskDetails details={netmaskDetails}></NetmaskDetails>
+          <NetmaskDetails details={netmaskDetails} issmall={""}></NetmaskDetails>
 
           <section>
             <div class="developer-links">
               <a href="https://github.com/dudash/cidr-finder"><img src={octocat} height="50" className="octocat" alt="github" /></a>
               <a href="https://developers.redhat.com"><img src={devlogo} height="50" className="devslogo" alt="devslink" /></a>
-              <a href="https://developers.redhat.com/products/rhoar/overview/"><img src={rhoar} height="50" className="rhoar" alt="rhoarlink" /></a>
             </div>
           </section>
 
@@ -128,11 +162,7 @@ class NetmaskDetails extends Component {
   render() {
     return (
       <div>
-        <div className="NetmaskDetails">
-          <span className="block">
-            <span className="value">{this.props.details.size.toLocaleString()}</span>
-            <span className="label">Available IPs</span>
-          </span>
+        <div className={"NetmaskDetails"+this.props.issmall}>
           <span className="block">
             <span className="value">{this.props.details.first}</span>
             <span className="label">Range Start</span>
@@ -141,8 +171,12 @@ class NetmaskDetails extends Component {
             <span className="value">{this.props.details.last}</span>
             <span className="label">Range End</span>
           </span>
+          <span className="block">
+            <span className="value">{this.props.details.size.toLocaleString()}</span>
+            <span className="label">Available IPs</span>
+          </span>
         </div>
-        <div className="NetmaskDetails">
+        <div className={"NetmaskDetails"+this.props.issmall}>
           <span className="block">
             <span className="value">{this.props.details.mask}</span>
             <span className="label">CIDR Netmask</span>
